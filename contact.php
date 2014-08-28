e8af5a3c356f3fe49ee77091bcbf94f33d6f75ef
Daniel Baigel <dbaigel1@gmail.com>
2014-08-28 12:26:24 -0400

    logo on contact

--- contact.php
+++ contact.php
@@ -18,14 +18,14 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'contact_page.html';
+		window.location = 'contact.html';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Message failed. Please, send an email to gordon@template-help.com');
-		window.location = 'contact_page.html';
+		window.location = 'contact.html';
 	</script>
 <?php
 }
